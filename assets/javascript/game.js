1ee97ad54c0062da2c9c1c42d2544bb72d339e12
eozuna3 <eo30239@gmail.com>
2020-02-12 21:40:54 -0600

    Update javascript to create a basic full functional page with some CSS styling.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 /*----------------------------Variables--------------------------------------------*/
 // Creating variables to hold the number of wins, losses, guesses left, random Letter chosen, and an array to hold the list of guesses made.
+var completedgame = true;
 var currentNamePos = 0;
 var wins = 0;
 var guessLeft = 12;
@@ -24,26 +25,26 @@ function randomPosition() {
 }
 
 // Function for placing selected letter in correct spot.
- function fillcurrentNameArray(letter){
-   for(z=0; z < currentNameArray.length; z++){
-    if(letter == currentName.charAt(z)){
+function fillcurrentNameArray(letter) {
+  for (z = 0; z < currentNameArray.length; z++) {
+    if (letter == currentName.charAt(z)) {
       currentNameArray[z] = currentName.charAt(z);
     }
-   }
- }
+  }
+}
 
- //Function for checking to see if selected letter was already used
-function alreadyGuessed(letter){
+//Function for checking to see if selected letter was already used
+function alreadyGuessed(letter) {
   for (y = 0; y < guessArray.length; y++) {
     if (letter == guessArray[y]) {
       return true;
     }
   }
-  return false; 
+  return false;
 }
 
 //Function for establishing the current name to be guessed and clearing the name array
-function selectNewName(position){
+function selectNewName(position) {
   currentName = characterNames[position];
   currentNameArray = [];
   for (x = 0; x < characterNames[position].length; x++) {
@@ -52,17 +53,21 @@ function selectNewName(position){
   console.log(currentName);
 }
 
-/*---------------------------Inital Establishment of Game-----------------------------*/
-//Initial Display Text
-winsText.textContent = "Wins: " + wins;
-guessesRemainingText.textContent = "Guesses left: " + guessLeft;
-guessedLettersText.textContent = "";
-instructionText.textContent = "Guess the name of the Stars Wars character below.  Push any letter to start the game.";
+function newGame() {
+  completedgame = true;
+  guessLeft = 12;
+  guessArray = [];
+  currentNamePos = randomPosition(characterNames);
+  selectNewName(currentNamePos);
+  instructionText.textContent = " Guess the name of the Stars Wars character below.  Push any letter to start playing the game, or press the spacebar to start a new game";
+  winsText.textContent = "Wins: " + wins;
+  currentWordText.textContent = currentNameArray.join(" ");
+  guessesRemainingText.textContent = "Guesses left: " + guessLeft;
+  guessedLettersText.textContent = "";
+}
 
-//Initializing the first name when the page loads and displaying a blank list of letters
-currentNamePos = randomPosition(characterNames.length);
-selectNewName(currentNamePos);
-currentWordText.textContent = currentNameArray.join(" ");
+/*---------------------------Inital Establishment of Game-----------------------------*/
+newGame();
 
 /*--------------------------Start of the game-------------------------------------*/
 document.onkeypress = function (event) {
@@ -75,53 +80,48 @@ document.onkeypress = function (event) {
   var userGuessKey2 = event.which;
 
   //Determine if user pressed lowercase letter
-  if ((userGuessKey1 >= 97 || userGuessKey2 >= 97) && (userGuessKey1 <= 122 || userGuessKey2 <= 122)){
-    
-    //Check to see if user has already guessed the letter, and if so do nothing, but if not then update the letters in the name
-    if (alreadyGuessed(userGuess) === false){
-      guessArray.push(userGuess);
-      guessLeft--;
-      fillcurrentNameArray(userGuess)
+  if ((userGuessKey1 >= 97 || userGuessKey2 >= 97) && (userGuessKey1 <= 122 || userGuessKey2 <= 122)) {
+    //Check to make sure current game has been completed and reset
+    if (completedgame === true) {
 
-      console.log(currentNameArray);
-      console.log(guessArray);
+      //Check to see if user has already guessed the letter, and if so do nothing, but if not then update the letters in the name
+      if (alreadyGuessed(userGuess) === false) {
+        guessArray.push(userGuess);
+        guessLeft--;
+        fillcurrentNameArray(userGuess)
+        console.log(currentNameArray);
+        console.log(guessArray);
 
-      //Check and see if they completed the word
-      if(currentNameArray.join("") === currentName){
-        currentWordText.textContent = currentNameArray.join(" ");
-        guessesRemainingText.textContent = "Guesses left: " + guessLeft;
-        guessedLettersText.textContent = guessArray.join(", ");
-        alert("Congrats, you won!!!!!");
-        
-        guessLeft = 12;
-        guessArray = [];
-        wins++;
-        currentNamePos = randomPosition(characterNames);
-        selectNewName(currentNamePos);
-        instructionText.textContent = " Guess the name of the Stars Wars character below.  Push any letter to start the game."; 
-        winsText.textContent = "Wins: " + wins;
-        currentWordText.textContent = currentNameArray.join(" ");
-        guessesRemainingText.textContent = "Guesses left: " + guessLeft;
-        guessedLettersText.textContent = "";
-        
+        //Check and see if they completed the word
+        if (currentNameArray.join("") === currentName) {
+          completedgame = false;
+          wins++;
+          winsText.textContent = "Wins: " + wins;
+          currentWordText.textContent = currentNameArray.join(" ");
+          guessesRemainingText.textContent = "Guesses left: " + guessLeft;
+          guessedLettersText.textContent = guessArray;
+          alert("Congrats, you won!!!!!  To play again please press space bar");
 
-        //If they don't complete the word, but have reached the end of their choices        
-      } else if (guessLeft > 0 ){
+          //If they don't complete the word, but have not reached the end of their choices        
+        } else if (guessLeft > 0) {
           currentWordText.textContent = currentNameArray.join(" ");
           guessesRemainingText.textContent = "Guesses left: " + guessLeft;
           guessedLettersText.textContent = guessArray.join(", ");
-      } else {
-        guessLeft = 12;
-        guessArray = [];
-        currentNamePos = randomPosition(characterNames);
-        selectNewName(currentNamePos);
-        instructionText.textContent = " Guess the name of the Stars Wars character below.  Push any letter to start the game."; 
-        winsText.textContent = "Wins: " + wins;
-        currentWordText.textContent = currentNameArray.join(" ");
-        guessesRemainingText.textContent = "Guesses left: " + guessLeft;
-        guessedLettersText.textContent = "";
-        alert("Sorry, you lose please try again");
+        
+        //They don't complete the word, and have run out of guesses  
+        } else {
+          completedgame = false;
+          winsText.textContent = "Wins: " + wins;
+          currentWordText.textContent = currentNameArray.join(" ");
+          guessesRemainingText.textContent = "Guesses left: " + guessLeft;
+          guessedLettersText.textContent = guessArray;
+          alert("Sorry, you lose. Please try again.  Press the space bar to start a new game");
+        }
       }
-    }  
-  }
+    } else {
+      alert("You need to restart a new game.  Please press the space bar to start a new game.");
+    }
+  } else if (userGuessKey1 == 32 || userGuessKey2 == 32) {
+    newGame();
+  } 
 }
